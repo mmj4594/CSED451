a4a2edfe0d43155bd2813447b63d37fb843283a0
mmj <mmj4594@naver.com>
2020-03-27 17:54:14 +0900

    Add basic driving parts

--- graphics_assn1/graphics_assn1/source.cpp
+++ graphics_assn1/graphics_assn1/source.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include "source.h"
+#include <stdlib.h>
+#include <time.h>
 
 bool allPass = false;
-bool allFail = true;
+bool allFail = false;
 
 using namespace std;
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(750, 500);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow(argv[0]);
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	glutIdleFunc(moveObjects);
+	glutIdleFunc(moveWall);
 	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(specialkeyboard);
 
@@ -26,25 +28,17 @@ int main(int argc, char** argv) {
 
 
 void init() {
+	srand(time(NULL));
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glShadeModel(GL_FLAT);
-	rectangle.x = rectangle.y = 0.0f;
-	rectangle.width = 0.1;
-	rectangle.height = 0.15;
-	
-	glBegin(GL_POLYGON);
-		glVertex2f(0, 0);
-		glVertex2f(0, 250);
-		glVertex2f(250, 250);
-		glVertex2f(250, 0);
-	glEnd();
-	
 }
 
 void display() {
-	drawTriangle();
-	drawSquare(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-	drawCircle(0.5 ,0.5, GREEN);
+	glClear(GL_COLOR_BUFFER_BIT);
+	drawSquare(0.0, 0.0, WORLD_X, 20, BLACK);														//floor
+	drawSquare(wall.getX(), wall.getY(), wall.getWidth(), wall.getHeight(), wall.getColor());		//wall
+	drawCircle(player.getX(), player.getY(), player.getRad(), player.getColor());					//player
+	drawCircle(thief.getX(), thief.getY(), thief.getRad(), thief.getColor());						//thief
 
 	glutSwapBuffers();			//백버퍼에 그림을 다 그렸으면, 전면버퍼와 통째로 교체한다.
 								//더블 버퍼에서는 프런트 버퍼 내용이 나오는 동안 새로운 내용이 백버퍼에 쓰이고,
@@ -56,14 +50,28 @@ void reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, 1, 0, 1);
+	gluOrtho2D(0, WORLD_X, 0, WORLD_Y);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
 
-void moveObjects() {
-	if(rectangle.x + rectangle.width < 1) rectangle.x += 0.001;
-	if(rectangle.y + rectangle.height < 1) rectangle.y += 0.001;
+void moveWall() {
+	wall.setX(wall.getX() - 0.3 * wallSpeed);
+	if (collisionCheck(&wall, &player)) {
+		if (allFail || wall.getColor() != player.getColor()) { cout << "Fail\n"; }
+		else if (allPass || wall.getColor() == player.getColor()) { cout << "Clear\n"; }
+	}
+	else if (collisionCheck(&wall, &thief)) {
+		wall.setColor(thief.getColor());
+	}
+	else if (collisionCheck(&player, &thief)) {
+		cout << "Win\n";
+	}
+
+	thief.setColor(rand() % 4);
+
+
+	if (wall.getX() < 0) wall = rect(WORLD_X, 20);
 
 	glutPostRedisplay();
 }
@@ -80,26 +88,20 @@ void setColor(int color) {
 		glColor3f(0.0, 0.0, 0.8);
 		break;
 	case YELLOW:
-		glColor3f(1.0, 1.0, 0.0);
+		glColor3f(0.9, 0.9, 0.0);
 		break;
 	case BLACK:
 		glColor3f(0.0, 0.0, 0.0);
+		break;
+	case GRAY:
+		glColor3f(0.3, 0.3, 0.3);
+		break;
 	}
 }
 
-void drawTriangle() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0f, 0.0f, 0.0f);
+void drawSquare(double x, double y, double width, double height, const int color) {
+	setColor(color);
 	glBegin(GL_POLYGON);
-		for (int i = 0; i < position.size(); i++) {
-			glVertex3f(position[i][0], position[i][1], position[i][2]);
-		}
-	glEnd();
-}
-
-void drawSquare(double x, double y, double width, double height) {
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_LINE_LOOP);
 		glVertex2f(x, y);
 		glVertex2f(x, y + height);
 		glVertex2f(x + width, y + height);
@@ -107,12 +109,11 @@ void drawSquare(double x, double y, double width, double height) {
 	glEnd();
 }
 
-void drawCircle(double x, double y, int color) {
+void drawCircle(double centerx, double centery, double rad, int color) {
 	setColor(color);
-	double rad = 0.05;
 	glBegin(GL_POLYGON);
 		for (float angle = 0; angle < 360; angle += 1.0) {
-			glVertex2f(x + rad * cos(angle), y + rad * sin(angle));
+			glVertex2f(centerx + rad * cos(angle), centery + rad * sin(angle));
 		}
 	glEnd();
 }
@@ -134,19 +135,38 @@ void keyboard(unsigned char key, int x, int y) {
 void specialkeyboard(int key, int x, int y) {
 	switch (key) {
 	case GLUT_KEY_UP:
-		rectangle.y += 0.005;
+		player.setColor(RED);
 		break;
 	case GLUT_KEY_DOWN:
-		rectangle.y -= 0.005;
+		player.setColor(GREEN);
 		break;
 	case GLUT_KEY_LEFT:
-		rectangle.x -= 0.005;
+		player.setColor(BLUE);
 		break;
 	case GLUT_KEY_RIGHT:
-		rectangle.x += 0.005;
+		player.setColor(YELLOW);
 		break;
 	}
 	glutPostRedisplay();
+}
+
+bool collisionCheck(object* a, object* b) {
+	//wall - player/thief collision check
+	if (a->getType() == 0) {
+		rect* wall = (rect*)a;
+		character* ch = (character*)b;
+
+		return ((wall->getX() < (ch->getX() + ch->getRad())) && (wall->getX() + wall->getWidth() > ch->getX()));
+	}
+
+	//player - thief collision check
+	else {
+		character* player = (character*)a;
+		character* thief = (character*)b;
+
+		return (player->getX() + player->getRad()) > (thief->getX() - thief->getRad());
+	}
+
 }
 
 //void glutTimerFunc(unsigned int millis, void (*func)(int value), int value); --> millis 후에 func를 호출하며 인수로 value를 전달.
--- graphics_assn1/graphics_assn1/source.h
+++ graphics_assn1/graphics_assn1/source.h
@@ -10,12 +10,56 @@ const int GREEN = 1;
 const int BLUE = 2;
 const int YELLOW = 3;
 const int BLACK = 4;
+const int GRAY = 5;
 
-typedef struct rect {
+const int WORLD_X = 150;
+const int WORLD_Y = 100;
+int wallSpeed = 1;
+
+typedef struct object {
+public:
+	int getType() { return type; }
+	int getColor() { return color; }
+	float getX() { return x; }
+	float getY() { return y; }
+
+	void setColor(const int newColor) { color = newColor; }
+	void setX(float newX) { x = newX; }
+	void setY(float newY) { y = newY; }	
+protected:
 	float x, y;
-	float width, height;
+	int color = GRAY;
+	int type;	//0 = wall, 1 = character
+} object;
+
+typedef struct rect: object {
+public:
+	rect(float a, float b) { type = 0; x = a; y = b; }
+
+	float getWidth() { return width; }
+	float getHeight() { return height; }
+
+	void setWidth(float newWidth) { width = newWidth; }
+	void setHeight(float newHeight) { height = newHeight; }
+
+private:
+	float width = 10, height = 50;
 } rect;
-rect rectangle;
+rect wall(WORLD_X, 20);
+
+typedef struct character: object {
+public:
+	character(float a, float b) { type = 1; x = a; y = b; }
+
+	double getRad() { return rad; }
+
+	void setRad(double newRad) { rad = newRad; }
+
+private:
+	double rad = 5.0;
+} character;
+character player(25, 25);
+character thief(75, 25);
 
 std::vector<glm::vec3> position = {
 	glm::vec3(0.0f, 0.0f, 0.0f),
@@ -26,13 +70,15 @@ std::vector<glm::vec3> position = {
 void init();
 void display();
 void reshape(int w, int h);
-void moveObjects();
+void moveWall();
 
 void setColor(int color);
 
 void drawTriangle();
-void drawSquare(double x, double y, double width, double height);
-void drawCircle(double x, double y, int color);
+void drawSquare(double x, double y, double width, double height, const int color);
+void drawCircle(double centerx, double centery, double rad, const int color);
 
 void keyboard(unsigned char key, int x, int y);
 void specialkeyboard(int key, int x, int y);
+
+bool collisionCheck(object* a, object* b);
