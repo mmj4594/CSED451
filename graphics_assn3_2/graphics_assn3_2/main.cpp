5ec3ab0d582a13addc296c546a1cdf82ea6e36e2
mmj <mmj4594@naver.com>
2020-05-18 16:47:57 +0900

    Minor fix in code

--- graphics_assn3_2/graphics_assn3_2/main.cpp
+++ graphics_assn3_2/graphics_assn3_2/main.cpp
@@ -39,9 +39,15 @@ int main(int argc, char** argv) {
 	init();
 	glutMainLoop();
 
+	//For safe unbound of VBO, VAO, EBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);			//VBO
+	glBindVertexArray(0);						//VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);	//EBO
+
 	//deallocate all resources when program ends
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 	return 0;
 }
@@ -95,24 +101,20 @@ void init() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-
 	//Linking Vertex Attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
 }
 
 void display3D() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	/*	Wireframe mode or Filling mode	*/
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
-	//glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 	glutSwapBuffers();
